15abca346cc1f1073734d0fb1d05eb9457ce6fc8
fernando <vluisfernando62@gmail.com>
2025-03-14 11:25:29 -0400

    chore: añade soporte para Tailwind CSS y actualiza estilos globales

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
