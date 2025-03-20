36f530b3e071b9ea6cbcfb81a2e985efaaf2b11a
fernando <vluisfernando62@gmail.com>
2025-03-20 15:17:04 -0400

    feat: agregar los estilos de tambler
    - Usar el estilo de la plantilla tabre
    - Usar los iconos de la plantilla

--- src/components/map/Info.jsx
+++ src/components/map/Info.jsx
@@ -1,12 +1,20 @@
 const Info = ({ region }) => {
   return (
-    <div className="relative rounded-2xl bg-1 shadow-lg overflow-hidden mx-auto">
-      <img src="./sucre.jpg" alt="text" className="w-full h-48 object-cover" />
+    <div className="col-md-6 col-lg-12 mx-auto">
+      <div className="card card-link card-link-rotate">
+        <div className="card-body">
+          <img
+            src="./sucre.jpg"
+            alt="text"
+            className="w-full h-48 object-cover"
+          />
 
-      <div className="p-4">
-        <h3 className="text-white font-bold text-3xl">{region?.name}</h3>
+          <div className="p-4">
+            <h3 className="text-dark font-bold">{region?.name}</h3>
 
-        <p className="text-sm text-white mt-2">{region?.description}</p>
+            <p className="text-sm text-dark mt-2">{region?.description}</p>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -8,19 +8,54 @@ const Map = () => {
   const handleRegionChange = (regionData) => {
     setSelectedRegion(regionData)
   }
-
   return (
-    <div className="bg-[#fcf2de] pb-16">
-      <section className="relative -mt-15 py-12 bg-white mx-auto px-4 rounded-2xl shadow-lg max-w-5/6">
-        <div className="container mx-auto px-4">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-            <Info region={selectedRegion} />
-            <MapSvg onRegionSelect={handleRegionChange} />
+    <div
+      className="sobre"
+      style={{
+        marginTop: '-150px',
+        position: 'relative',
+        zIndex: 10, // Asegura que esté por encima
+      }}>
+      <div className="container-xl">
+        <div className="row row-deck row-cards">
+          <div className="col-sm-12 col-lg-12">
+            <div
+              className="card shadow-lg"
+              style={{
+                top: '50px',
+                left: '50%',
+                transform: 'translateX(-50%)',
+                zIndex: 30,
+              }}>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-6">
+                    <Info region={selectedRegion} />
+                  </div>
+                  <div className="col-6">
+                    <MapSvg onRegionSelect={handleRegionChange} />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </section>
+      </div>
     </div>
   )
+
+  // return (
+  //   <div className="bg-[#fcf2de] pb-16">
+  //     <section className="relative -mt-15 py-12 bg-white mx-auto px-4 rounded-2xl shadow-lg max-w-5/6">
+  //       <div className="container mx-auto px-4">
+  //         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+  //           <Info region={selectedRegion} />
+  //           <MapSvg onRegionSelect={handleRegionChange} />
+  //         </div>
+  //       </div>
+  //     </section>
+  //   </div>
+  // )
 }
 
 export default Map
